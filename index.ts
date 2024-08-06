f9f85dc1cedbc408da0b4070bce1f85af1ecf1c5
MikhilMC <mikhilmohan13@gmail.com>
2024-08-06 13:27:30 +0530

    error in code example

---
+++ index.ts
@@ -0,0 +1,16 @@
+const students = [
+  {
+    name: "John",
+    mark: 70,
+  },
+  {
+    name: "Mark",
+    mark: 90,
+  },
+];
+
+for (const student of students) {
+  console.log("Name: ", student.name);
+  console.log("Mark: ", student.mark);
+  console.log("Grade: ", student.grade);
+}
