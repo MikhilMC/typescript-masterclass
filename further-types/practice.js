7b343adbccb9cd40d5cc35d9f73b3d0129bc13bc
MikhilMC <mikhilmohan13@gmail.com>
2024-08-30 16:52:20 +0530

    practice on further more types in typescript is completed

---
+++ further-types/practice.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let city = "New York"; // string
+let population = 8400000; // number
+const age = 32; // 32
+let oldAge = 79; // 79
+let newAge = oldAge; // 79
+let data = new Map(); // Map<any, any>
+let score = [90, 86, 100];
---
+++ further-types/practice.ts
@@ -0,0 +1,25 @@
+let city: string = "New York"; // string
+let population: number = 8400000; // number
+const age = 32; // 32
+let oldAge = 79 as const; // 79
+let newAge = oldAge; // 79
+let data = new Map(); // Map<any, any>
+let score: number[] = [90, 86, 100];
+type Primitive = string | number | boolean; // string | number | boolean
+type CustomName = "John" extends string ? string : "John"; // string
+type CustomAge = typeof newAge extends number ? 79 : number; // 79
+type CheckData = typeof data extends Object ? true : false; // true
+type CheckScore = typeof score extends never ? {} : []; // []
+
+/*
+ * Are the following statements valid
+ * Check if below lines of code are valid as per TypeScript or not without uncommenting them
+ */
+
+// age = 85;    //! invalid
+// score.push(10);  //* valid
+// score.push("New Score"); //! invalid
+// let customAge: CustomAge = 50;   //! invalid
+// let primitive: Primitive = new Date();   //! invalid
+// let years: CheckScore = [];  //* valid
+export {};
