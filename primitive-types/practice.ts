6fad068253f245e001d5751e1616f88b8434090f
MikhilMC <mikhilmohan13@gmail.com>
2024-08-30 16:26:51 +0530

    completed practice for primitive types

---
+++ primitive-types/practice.ts
@@ -0,0 +1,13 @@
+/**
+ * Assign the correct types to each of the following variables
+ * Solution
+ */
+let message: string = "Hello, TypeScript!";
+let age: number = 42;
+let isStudent: boolean = true;
+let fetched: null = null;
+let user: undefined = undefined;
+let largeNumber: bigint = 9007199254740991n;
+let unique: symbol = Symbol("uniqueSymbol");
+
+export {};
